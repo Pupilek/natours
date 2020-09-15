8bd1156b5a075c9ba49f79dd59b12c57c4b508e4
Andrii Loburets <loburets.andrii@gmail.com>
2020-09-15 22:56:39 +0200

    ending making the API better

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,15 +1,39 @@
 const { nextTick } = require('process');
 const Tour = require('../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price, ratingsAverage, summary, difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
-    //1.Build query
+    console.log(req.query);
+
+    //BUILD QUERY
+
+    //1. Filtering
+    //1.1 Basic filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => {
       delete queryObj[el];
     });
-    const query = Tour.find(queryObj);
+
+    //1.2 Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+    // gt, gte, lt, lte
+    // {
+    //   duration: { gte: '5' },
+    //   difficulty: 'easy',
+    //   page: '2',
+    //   sort: '2',
+    //   limit: '10'
+    // }
 
     // const query = await Tour.find()
     //   .where('duration')
@@ -17,7 +41,41 @@ exports.getAllTours = async (req, res) => {
     //   .where('difficulty')
     //   .equals('easy');
 
-    //2.execute query
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2 Sorting
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+    //sort('price ratingsAverage')
+
+    // 3 Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //4.Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
+    //EXECUTE QUERY
     const tours = await query;
 
     //3. Send response
